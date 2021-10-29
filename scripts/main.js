1185aa7a0f828ad150041fdb2da7ba07f746e254
Danila-Developer <79847859+Danila-Developer@users.noreply.github.com>
2021-10-29 14:55:20 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
+let popap = document.querySelector('.popap');
+let buttons = document.querySelectorAll('.service__button');
+buttons.forEach((item)=>{
+   item.addEventListener('click', ()=>{
+      popap.classList.add('active');
+   });
+});
+
+let close = document.querySelector('.close_i');
+close.addEventListener('click', ()=>{
+   popap.classList.remove('active');
+});
 $('.slider').slick();
